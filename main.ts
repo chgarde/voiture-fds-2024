73ab87602da7b92a0d54cc0a9b1762665e7e9d3c
foxyd611 <garde.jeremi@gmail.com>
2024-10-01 17:23:55 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Yes)
 })
 bluetooth.onBluetoothDisconnected(function () {
     reset()
@@ -10,34 +10,10 @@ function stopMoteurDirection () {
     servos.P1.stop()
 }
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
-    traitemementMessage()
+	
 })
 function commandeMoteurGauche () {
     servos.P0.run(puissanceRotation)
-}
-function traitemementMessage () {
-    message = bluetooth.uartReadUntil(serial.delimiters(Delimiters.SemiColon))
-    if (message == "down_haut") {
-        moteur = 1
-        tournerHoraire = 1
-    } else if (message == "up_haut") {
-        tournerHoraire = 0
-    } else if (message == "down_bas") {
-        moteur = 1
-        tournerAntiHoraire = 1
-    } else if (message == "up_bas") {
-        tournerAntiHoraire = 0
-    } else if (message == "down_droite") {
-        moteur = 0
-        tournerHoraire = 1
-    } else if (message == "up_droite") {
-        tournerHoraire = 0
-    } else if (message == "down_gauche") {
-        moteur = 0
-        tournerAntiHoraire = 1
-    } else if (message == "up_gauche") {
-        tournerAntiHoraire = 0
-    }
 }
 function traitemementMessage2 () {
     message = bluetooth.uartReadUntil(serial.delimiters(Delimiters.SemiColon))
@@ -67,34 +43,23 @@ function stopMoteur () {
     servos.P2.stop()
 }
 function commandeMoteurReculer () {
-    if (tournerHoraire == 1) {
-        servos.P2.run(0 - puissanceMoteur)
-        servos.P1.run(0 - puissanceMoteur)
-    }
+    servos.P2.run(0 - puissanceMoteur)
+    servos.P1.run(0 - puissanceMoteur)
 }
 function reset () {
     puissanceMoteur = 100
     puissanceRotation = 50
 }
 function commandeMoteurAvancer () {
-    if (tournerHoraire == 1) {
-        servos.P2.run(puissanceMoteur)
-        servos.P1.run(puissanceMoteur)
-    }
+    servos.P2.run(puissanceMoteur)
+    servos.P1.run(puissanceMoteur)
 }
 let puissanceMoteur = 0
-let tournerAntiHoraire = 0
-let tournerHoraire = 0
-let moteur = 0
 let message = ""
 let puissanceRotation = 0
 bluetooth.startUartService()
 basic.showIcon(IconNames.Square)
 reset()
 basic.forever(function () {
-    if (moteur == 0) {
-        commandeMoteurDroite()
-    } else {
-        commandeMoteurAvancer()
-    }
+	
 })
